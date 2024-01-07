60dd66bdbe7dd9b31ae7104133c72787717f7f48
Daniel-Burbridge-Developer <Daniel.John.Developer@gmail.com>
2024-01-07 19:38:09 +0800

    Completed Tutorial

--- Mazesolver/tests.py
+++ Mazesolver/tests.py
@@ -1,5 +1,5 @@
 import unittest
-from main import Maze
+from Main import Maze
 
 class Tests(unittest.TestCase):
     def test_maze_create_cells(self):
@@ -8,11 +8,23 @@ class Tests(unittest.TestCase):
         m1 = Maze(0,0,num_rows, num_cols, 10, 10)
         self.assertEqual(
             len(m1._cells),
-            num_cols,
+            num_rows,
         )
         self.assertEqual(
             len(m1._cells[0]),
-            num_rows,
+            num_cols,
         )
+
+    def test_maze_break_entrance_and_exit(self):
+        num_cols = 12
+        num_rows = 10
+        m1 = Maze(0,0,num_rows, num_cols, 10, 10)
+        self.assertFalse(
+            m1._cells[0][0].has_top_wall,
+        )
+        self.assertFalse(
+            m1._cells[-1][-1].has_bottom_wall,
+        )
+
 if __name__ == '__main__':
     unittest.main()
